27d5c4798f23047ad26a1e1e3bfb6cec3bcb12c8
Andrian2024 <andrianborsuck@cs.colman.ac.il>
2024-09-02 22:26:21 +0300

    added product schema and product &user CRUD

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors"); //ADD LATER FOR SECURITY MEASURES
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
 const config = require("./config/config");
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 connectDB();
 
 app.use(config.api.prefix, userRoutes);
+app.use(config.api.prefix, productRoutes);
 
 app.listen(config.server.port, () => {
   console.log(`Server is Listening on Port ${config.server.port}`);
