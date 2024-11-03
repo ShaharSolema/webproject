b92fc0e81381680e8c602cc0be5b349f4a4a9fdb
Andrian2024 <andrianborsuck@gmail.com>
2024-11-03 23:29:24 +0200

    revert changes on highlight

--- client/src/components/Home/Highlight.jsx
+++ client/src/components/Home/Highlight.jsx
@@ -2,61 +2,20 @@ import React, { useState, useRef, useEffect } from 'react';
 import '../../styles/Highlight.css';
 
 // ייבוא התמונות והסרטונים
-import Courses1 from '../../styles/pictures/Courses/VID-20241025-WA0020.mp4';
-import Courses2 from '../../styles/pictures/Courses/VID-20241025-WA0021.mp4';
+import Courses1 from '../../styles/pictures/Courses/One.png';
+import Courses2 from '../../styles/pictures/Courses/VID-20241025-WA0020.mp4';
 import Courses3 from '../../styles/pictures/Courses/VID-20241025-WA0022.mp4';
 import Courses4 from '../../styles/pictures/Courses/VID-20241025-WA0023.mp4';
-import Courses6 from '../../styles/pictures/Courses/VID-20241025-WA0024.mp4';
-import Courses5 from '../../styles/pictures/Courses/VID-20241025-WA0025.mp4';
-import Courses7 from '../../styles/pictures/Courses/VID-20241025-WA0026.mp4';
-import Guides1 from '../../styles/pictures/Guides/VID-20240905-WA0011.mp4';
-import Guides2 from '../../styles/pictures/Guides/VID-20240905-WA0012.mp4';
-import Guides3 from '../../styles/pictures/Guides/VID-20240905-WA0013.mp4';
-import Guides4 from '../../styles/pictures/Guides/VID-20240905-WA0014.mp4';
-import Guides5 from '../../styles/pictures/Guides/WhatsApp Video 2024-11-03 at 20.58.16.mp4';
-import Thespringbrushes1 from '../../styles/pictures/Thespringbrushes/VID-20240905-WA0010.mp4';
-import Thespringbrushes2 from '../../styles/pictures/Thespringbrushes/VID-20240905-WA0009.mp4';
-import Thespringbrushes3 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0019.jpg';
-import Thespringbrushes4 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0020.jpg';
-import Thespringbrushes5 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0021.jpg';
-import Thespringbrushes6 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0022.jpg';
-import Thespringbrushes7 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0023.jpg';
-import Thespringbrushes8 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0024.jpg';
-import Thespringbrushes9 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0025.jpg';
-import Examples1 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.22 (1).jpeg';
-import Examples30 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.22 (2).jpeg';
-import Examples2 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.23 (1).jpeg';
-import Examples3 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.23.jpeg';
-import Examples4 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.24 (1).jpeg';
-import Examples5 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.24 (2).jpeg';
-import Examples6 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.24.jpeg';
-import Examples7 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.25.jpeg';
-import Examples8 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.26 (1).jpeg';
-import Examples9 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.26.jpeg';
-import Examples10 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.27 (1).jpeg';
-import Examples11 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.27 (2).jpeg';
-import Examples12 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.27.jpeg';
-import Examples13 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.28.jpeg';
-import Examples14 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.29 (1).jpeg';
-import Examples15 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.29 (2).jpeg';
-import Examples16 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.29.jpeg';
-import Examples17 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.30.jpeg';
-import Examples18 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.31.jpeg';
-import Examples19 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.32.jpeg';
-import Examples20 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.33 (1).jpeg';
-import Examples21 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.33 (2).jpeg';
-import Examples22 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.33.jpeg';
-import Examples23 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.33.jpeg';
-import Examples24 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.34.jpeg';
-import Examples25 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.35.jpeg';
-import Examples26 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.35.jpeg';
-import Examples27 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.36.jpeg';
-import Examples28 from '../../styles/pictures/Examples/WhatsApp Image 2024-11-03 at 22.21.36 (1).jpeg';
-
-
-
-
-
+import Courses5 from '../../styles/pictures/Courses/VID-20241025-WA0023.mp4';
+import Courses6 from '../../styles/pictures/Courses/VID-20241025-WA0020.mp4';
+import Courses7 from '../../styles/pictures/Courses/VID-20241025-WA0025.mp4';
+import Courses8 from '../../styles/pictures/Courses/VID-20241025-WA0026.mp4';
+import Guides1 from '../../styles/pictures/Guides/gone.png';
+import Guides2 from '../../styles/pictures/Guides/VID-20240905-WA0011.mp4';
+import Guides3 from '../../styles/pictures/Guides/VID-20240905-WA0012.mp4';
+import Guides4 from '../../styles/pictures/Guides/VID-20240905-WA0013.mp4';
+import Thespringbrushes1 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0019.jpg';
+import Thespringbrushes2 from '../../styles/pictures/Thespringbrushes/IMG-20240905-WA0021.jpg';
 
 const Highlight = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -67,13 +26,12 @@ const Highlight = () => {
 
   // מערך עם התמונות והסרטונים
   const media = [
-    [Courses1, Courses2, Courses3, Courses4, Courses5, Courses6,Courses7], // קורסים
-    [Guides1, Guides2, Guides3, Guides4,Guides5], // הדרכות
-    [Thespringbrushes1, Thespringbrushes2,Thespringbrushes3,Thespringbrushes4,Thespringbrushes5,Thespringbrushes6,Thespringbrushes7,Thespringbrushes8,Thespringbrushes9], // מכחולים
-    [Examples1,Examples2,Examples3,Examples4,Examples5,Examples6,Examples7,Examples8,Examples9,Examples10,Examples11,Examples12,Examples13,Examples14,Examples15,Examples16,Examples17,Examples18,Examples19,Examples20,Examples21,Examples22,Examples23,Examples24,Examples25,Examples26,Examples27,Examples28,Examples30],
+    [Courses1, Courses2, Courses3, Courses4, Courses5, Courses6, Courses7, Courses8], // קורסים
+    [Guides1, Guides2, Guides3, Guides4], // הדרכות
+    [Thespringbrushes1, Thespringbrushes2], // מכחולים
   ];
 
-  const titles = ['קורסים', 'הדרכות', 'מכחולים','דוגמאות להשראה'];
+  const titles = ['קורסים', 'הדרכות', 'מכחולים'];
 
   const handleCircleClick = (index) => {
     setCurrentGalleryIndex(index);
